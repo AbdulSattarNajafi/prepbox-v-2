1f897fa53b7bb67520b12eeba7d0781eeab9bcbe
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-24 10:47:08 +0700

    hero image lazyloaded

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react';
+
+import HeroImage from './../../assets/images/png/home-hero.png';
 import PrimaryButton from '../button/Primary-button';
 import classes from './Hero.module.css';
 
 const Hero = () => {
+    const [loadedHeroBg, setLoadedHeroBg] = useState(null);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = HeroImage;
+        img.onload = () => setLoadedHeroBg(HeroImage);
+    }, []);
+
     return (
-        <section className={classes.hero}>
+        <section className={classes.hero} style={{ backgroundImage: `url(${loadedHeroBg})` }}>
             <div className='container'>
                 <div className={classes['hero__content']}>
                     <h1>
