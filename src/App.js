753d418419619c5ae53885c64b83b4950b57c944
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-27 16:02:52 +0700

    code splited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Layout from './layout/Layout';
-import Home from './pages/Home';
-import Tutor from './pages/Tutor';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
+import Loader from './components/elements/loader';
 import NotFound from './pages/NotFound';
+
+const HomePage = React.lazy(() => import('./pages/Home'));
+const HomePageJon = React.lazy(() => import('./pages/Home_Jon'));
+const TutorPage = React.lazy(() => import('./pages/Tutor'));
+const BlogPage = React.lazy(() => import('./pages/Blog'));
+const BlogPostPage = React.lazy(() => import('./pages/BlogPost'));
 
 const router = createBrowserRouter([
     {
@@ -15,19 +19,43 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Home />,
+                element: (
+                    <Suspense fallback={<Loader />}>
+                        <HomePage />
+                    </Suspense>
+                ),
             },
             {
                 path: 'tutor',
-                element: <Tutor />,
+                element: (
+                    <Suspense fallback={<Loader />}>
+                        <TutorPage />
+                    </Suspense>
+                ),
             },
             {
                 path: 'blog',
-                element: <Blog />,
+                element: (
+                    <Suspense fallback={<Loader />}>
+                        <BlogPage />
+                    </Suspense>
+                ),
             },
             {
                 path: 'blog/:postId',
-                element: <BlogPost />,
+                element: (
+                    <Suspense fallback={<Loader />}>
+                        <BlogPostPage />
+                    </Suspense>
+                ),
+            },
+            {
+                path: 'signups',
+                element: (
+                    <Suspense fallback={<Loader />}>
+                        <HomePageJon />
+                    </Suspense>
+                ),
             },
         ],
     },
