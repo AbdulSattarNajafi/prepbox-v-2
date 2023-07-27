2796e34fb07f3449dd9d779fd59de9a2beb3460b
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-27 16:46:00 +0700

    hero image updated

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,3 +1,6 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import HeroImage from './../../assets/images/png/home-hero.png';
+
 import PrimaryButton from '../button/Primary-button';
 import classes from './Hero.module.css';
 
@@ -12,6 +15,10 @@ const Hero = () => {
                     <p>Guaranteed improvement in math grades</p>
                     <PrimaryButton text='Try for free' />
                 </div>
+            </div>
+
+            <div className={classes['hero__image']}>
+                <LazyLoadImage src={HeroImage} width='1400' height='800' alt='Banner' />
             </div>
         </section>
     );
