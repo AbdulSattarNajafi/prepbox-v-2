3087966138476c89217ec71a33b5717ee269bda5
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-31 13:14:58 +0700

    Hero images added with Webp format

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -3,9 +3,14 @@
 import HeroImage from './../../assets/images/png/home-hero.png';
 import HeroImageWebp from './../../assets/images/webp/home-hero.webp';
 
-// import HeroImageMd from './../../assets/images/png/home-hero-md.png';
-// import HeroImageSm from './../../assets/images/png/home-hero-sm.png';
-// import HeroImageXs from './../../assets/images/png/home-hero-xs.png';
+import HeroImageMd from './../../assets/images/png/home-hero-md.png';
+import HeroImageMdWebp from './../../assets/images/webp/home-hero-md.webp';
+
+import HeroImageSm from './../../assets/images/png/home-hero-sm.png';
+import HeroImageSmWebp from './../../assets/images/webp/home-hero-sm.webp';
+
+import HeroImageXs from './../../assets/images/png/home-hero-xs.png';
+import HeroImageXsWebp from './../../assets/images/webp/home-hero-xs.webp';
 
 import PrimaryButton from '../button/Primary-button';
 import classes from './Hero.module.css';
@@ -24,7 +29,7 @@ const Hero = () => {
             </div>
 
             <div className={classes['hero__image']}>
-                <picture>
+                <picture className={classes['hero__image-lg']}>
                     <source type='image/webp' srcSet={HeroImageWebp} />
                     <img src={HeroImage} width='1120' height='622' alt='Banner' />
                 </picture>
@@ -35,22 +40,40 @@ const Hero = () => {
                     width='1120'
                     height='622'
                     alt='Banner'
-                />
-                <LazyLoadImage
+                /> */}
+
+                <picture className={classes['hero__image-md']}>
+                    <source type='image/webp' srcSet={HeroImageMdWebp} />
+                    <img src={HeroImageMd} width='860' height='593' alt='Banner' />
+                </picture>
+
+                {/* <LazyLoadImage
                     className={classes['hero__image-md']}
                     src={HeroImageMd}
                     width='860'
                     height='593'
                     alt='Banner'
-                />
-                <LazyLoadImage
+                /> */}
+
+                <picture className={classes['hero__image-sm']}>
+                    <source type='image/webp' srcSet={HeroImageSmWebp} />
+                    <img src={HeroImageSm} width='440' height='550' alt='Banner' />
+                </picture>
+
+                {/* <LazyLoadImage
                     className={classes['hero__image-sm']}
                     src={HeroImageSm}
                     width='440'
                     height='550'
                     alt='Banner'
-                />
-                <LazyLoadImage
+                /> */}
+
+                <picture className={classes['hero__image-xs']}>
+                    <source type='image/webp' srcSet={HeroImageXsWebp} />
+                    <img src={HeroImageXs} width='320' height='582' alt='Banner' />
+                </picture>
+
+                {/* <LazyLoadImage
                     className={classes['hero__image-xs']}
                     src={HeroImageXs}
                     width='320'
