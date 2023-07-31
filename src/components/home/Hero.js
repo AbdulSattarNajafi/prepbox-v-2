c885943158e98ef1229cebe6b3e794613b96249f
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-31 13:52:06 +0700

    hero images modified

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,15 +1,15 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import HeroImage from './../../assets/images/png/home-hero.png';
-// import HeroImageWebp from './../../assets/images/webp/home-hero.webp';
+import HeroImageWebp from './../../assets/images/webp/home-hero.webp';
 
-import HeroImageMd from './../../assets/images/png/home-hero-md.png';
+// import HeroImageMd from './../../assets/images/png/home-hero-md.png';
 // import HeroImageMdWebp from './../../assets/images/webp/home-hero-md.webp';
 
-import HeroImageSm from './../../assets/images/png/home-hero-sm.png';
+// import HeroImageSm from './../../assets/images/png/home-hero-sm.png';
 // import HeroImageSmWebp from './../../assets/images/webp/home-hero-sm.webp';
 
-import HeroImageXs from './../../assets/images/png/home-hero-xs.png';
+// import HeroImageXs from './../../assets/images/png/home-hero-xs.png';
 // import HeroImageXsWebp from './../../assets/images/webp/home-hero-xs.webp';
 
 import PrimaryButton from '../button/Primary-button';
@@ -29,6 +29,17 @@ const Hero = () => {
             </div>
 
             <div className={classes['hero__image']}>
+                <picture>
+                    <source type='image/webp' srcSet={HeroImageWebp} />
+                    <img
+                        fetchpriority='high'
+                        src={HeroImage}
+                        width='1120'
+                        height='622'
+                        alt='Banner'
+                    />
+                </picture>
+
                 {/* <picture className={classes['hero__image-lg']}>
                     <source type='image/webp' srcSet={HeroImageWebp} />
                     <img
@@ -40,40 +51,40 @@ const Hero = () => {
                     />
                 </picture> */}
 
-                <LazyLoadImage
+                {/* <LazyLoadImage
                     className={classes['hero__image-lg']}
                     src={HeroImage}
                     fetchpriority='high'
                     width='1120'
                     height='622'
                     alt='Banner'
-                />
+                /> */}
 
                 {/* <picture className={classes['hero__image-md']}>
                     <source type='image/webp' srcSet={HeroImageMdWebp} />
                     <img src={HeroImageMd} width='860' height='593' alt='Banner' />
                 </picture> */}
 
-                <LazyLoadImage
+                {/* <LazyLoadImage
                     className={classes['hero__image-md']}
                     src={HeroImageMd}
                     width='860'
                     height='593'
                     alt='Banner'
-                />
+                /> */}
 
                 {/* <picture className={classes['hero__image-sm']}>
                     <source type='image/webp' srcSet={HeroImageSmWebp} />
                     <img src={HeroImageSm} width='440' height='550' alt='Banner' />
                 </picture> */}
 
-                <LazyLoadImage
+                {/* <LazyLoadImage
                     className={classes['hero__image-sm']}
                     src={HeroImageSm}
                     width='440'
                     height='550'
                     alt='Banner'
-                />
+                /> */}
 
                 {/* <picture className={classes['hero__image-xs']}>
                     <source type='image/webp' srcSet={HeroImageXsWebp} />
@@ -86,13 +97,13 @@ const Hero = () => {
                     />
                 </picture> */}
 
-                <LazyLoadImage
+                {/* <LazyLoadImage
                     className={classes['hero__image-xs']}
                     src={HeroImageXs}
                     width='320'
                     height='582'
                     alt='Banner'
-                />
+                /> */}
             </div>
         </section>
     );
