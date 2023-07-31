cf169db5ab04207a79f5989d84371466914a31a3
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2023-07-31 14:28:47 +0700

    Hero Imported as lazy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-import Hero from '../components/home/Hero';
+import React, { lazy } from 'react';
+// import Hero from '../components/home/Hero';
 import Admission from '../components/home/Admission';
 import HowWork from '../components/home/HowWork';
 import Support from '../components/home/Support';
 import Plans from '../components/home/Plans';
 import Session from '../components/home/Session';
+const HeroComponent = lazy(() => import('../components/home/Hero'));
 
 const Home = () => {
     return (
         <>
-            <Hero />
+            <HeroComponent />
             <Admission />
             <HowWork />
             <Support />
